e327473c57174f71c6a10ed3699fbe4aa9eb453a
Manasa Rao <drmanasa18.rao@gmail.com>
2019-12-04 14:07:38 +0100

    adds login unit tests

---
+++ features/step_definitions/background_steps.rb
@@ -0,0 +1,11 @@
+Given("I am on the Login page") do
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Given("the following user exists") do |table|
+    # table is a Cucumber::MultilineArgument::DataTable
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  
+  
---
+++ features/step_definitions/basic_steps.rb
@@ -0,0 +1,13 @@
+Then("I fill {string} with {string}") do |string, string2|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then("I fill in {string} with {string}") do |string, string2|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  Then("I click {string}") do |string|
+    pending # Write code here that turns the phrase above into concrete actions
+  end
+  
+  
